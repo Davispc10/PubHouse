f282bf990da0e8458f4ed8c51671fea275250dfe
Davispc10 <davispc100@gmail.com>
2017-03-30 10:02:27 -0300

    ajustes upload image

--- app/uploaders/foto_produto_uploader.rb
+++ app/uploaders/foto_produto_uploader.rb
@@ -1,5 +1,4 @@
 class FotoProdutoUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -10,8 +9,13 @@ class FotoProdutoUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def cache_dir
+    "#{Rails.root}/uploads/tmp/"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -23,7 +27,7 @@ class FotoProdutoUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
- # process scale: [200, 300]
+ # process scale: [100, 100]
   #
   # def scale(width, height)
   #   # do something
@@ -36,9 +40,9 @@ class FotoProdutoUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
